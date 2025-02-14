630a5dce9bf4c952ee308040f303a12dca31957f
Nonsense727 <amarmend.11b@gmail.com>
2025-02-14 13:33:22 +0800

    commit first bituumjleltei bolgoj private zarlaltuud holbod mains handah erhte blsn

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape shape1 = new Rectangle(2, 3);
+        Shape shape2 = new Square(4);
+        Shape shape3 = new Circle(5);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(shape1);
+        Renderer renderer2 = new Renderer(shape2);
+        Renderer renderer3 = new Renderer(shape3);
 
-        renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
